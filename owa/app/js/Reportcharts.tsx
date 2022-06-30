0acf73d1c85cc4ba97f632ea771d1a0f5fbd9711
Marcin Kowalski <marcinkowalski189@gmail.com>
2022-06-30 15:08:32 +0200

    [maven-release-plugin] prepare release 1.0.1

---
+++ owa/app/js/Reportcharts.tsx
@@ -0,0 +1,47 @@
+/* * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+
+import React from 'react';
+import Routes from './Routes';
+import initStore, { history } from './config/redux-store';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { toast } from 'react-toastify';
+import { loadIcons } from './config/icon-loader';
+import { IntlProvider } from 'react-intl';
+import en from './translations/en.json';
+import flatten from 'flat';
+import 'react-toastify/dist/ReactToastify.css';
+import '@openmrs/style-referenceapplication/lib/referenceapplication.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-3/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../css/font-awesome.min.css';
+import '../css/Inputs.scss';
+import '../css/Reportcharts.scss';
+import 'babel-polyfill';
+
+loadIcons();
+toast.configure();
+const store = initStore();
+const messages = flatten(en);
+const locale = 'en';
+
+render(
+  <React.StrictMode>
+    <IntlProvider locale={locale} messages={messages}>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          {Routes()}
+        </ConnectedRouter>
+      </Provider>
+    </IntlProvider>
+  </React.StrictMode>
+, document.getElementById('app'));
---
+++ owa/app/js/Routes.tsx
@@ -0,0 +1,36 @@
+/* * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+
+import React from 'react'
+import LandingPage from './components/LandingPage';
+import Customize from './components/customize/customize'
+import Breadcrumbs from './components/breadcrumbs/Breadcrumbs';
+import DataVisualization from './components/data-visualization/DataVisialization';
+import DataVisualizationConfiguration from './components/data-visualization-configuration/DataVisualizationConfiguration';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header } from '@openmrs/react-components';
+
+const Routes = () => (
+  <>
+    <Customize />
+    <Header />
+    <Breadcrumbs />
+    <div className="body-wrapper">
+      <Router>
+        <Switch>
+          <Route exact path="/configuration" component={DataVisualizationConfiguration} />
+          <Route exact path="/visualization" component={DataVisualization} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>
+    </div>
+  </>
+);
+
+export default Routes;
