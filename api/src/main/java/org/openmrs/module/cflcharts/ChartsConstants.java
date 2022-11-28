c25a70989f0794134f27c167cf62469b9b9705ae
druchniewicz <druchniewicz@soldevelo.com>
2022-11-28 17:17:36 +0100

    AGRE-2626: Moved chart priviliges creation to config.xml file

--- api/src/main/java/org/openmrs/module/cflcharts/ChartsConstants.java
+++ api/src/main/java/org/openmrs/module/cflcharts/ChartsConstants.java
@@ -10,14 +10,6 @@ public final class ChartsConstants {
 
   public static final String REPORTS_FOR_DATA_VISUALIZATION_CONFIGURATION_PROPERTY_DEFAULT_VALUE = "[]";
 
-  public static final String VIEW_CHARTS_PRIVILEGE_NAME = "View Charts";
-
-  public static final String VIEW_CHARTS_PRIVILEGE_DESCRIPTION = "Able to see charts page";
-
-  public static final String CONFIGURE_CHARTS_PRIVILEGE_NAME = "Configure Charts";
-
-  public static final String CONFIGURE_CHARTS_PRIVILEGE_DESCRIPTION = "Able to configure charts";
-
   private ChartsConstants() {
     // constants-only class
   }
